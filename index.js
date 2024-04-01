3a3f3b20f21bb3e334bc05937ccc2f301b1261f0
Sarah Naunton <sarah@naunton.org>
2024-04-01 17:03:28 +0100

    Set up index.js and knex file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+const { CORS_ORIGIN } = process.env
+const cors = require("cors")
+app.use(cors(CORS_ORIGIN))
+app.use(express.json())
+
+app.listen(port, () => {
+console.log(`running at http://localhost:${port}`)
+})
